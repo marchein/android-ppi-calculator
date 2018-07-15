3ffdf953a4ef20aff14c5ce983dbe06a549af982
Marc Hein <info@marc-hein.de>
2018-07-16 00:12:37 +0200

    Minor tweaks
    - Replaced EditText for result with TextView
    - Added proper rounding solution

--- app/src/main/java/de/marc_hein/ppicalculator/MainActivity.java
+++ app/src/main/java/de/marc_hein/ppicalculator/MainActivity.java
@@ -9,7 +9,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Vector;
@@ -19,10 +21,14 @@ import javax.xml.datatype.Duration;
 public class MainActivity extends AppCompatActivity {
 
     public static final String DEBUG_TAG = "HEIN_DEBUG";
+    private String resultDefaultText;
     private double resultValue;
     private long heightValue, widthValue;
     private float sizeValue;
     private ArrayList<EditText> textFields;
+    private TextView resultField;
+    private boolean roundResult;
+    private boolean hasResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,28 +38,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onCheckBoxClicked(View v) {
-        boolean checked = ((CheckBox) v).isChecked();
-        switch (v.getId()) {
-            case R.id.toggleRounding:
-                if (checked) {
-                    Log.d(DEBUG_TAG, "round!");
-                } else {
-                    Log.d(DEBUG_TAG, "don't round!");
-                }
-                break;
+        roundResult = ((CheckBox) v).isChecked();
+        if (hasResult) {
+            resultField.setText(formatResult(getResult(resultValue)));
         }
     }
 
     public void onCalculateClick(View v) {
         Log.d(DEBUG_TAG, "Calculating...");
         String[] currentValues = new String[3];
-        for (int i = 0; i < textFields.size() - 1; i++) {
+
+        for (int i = 0; i < textFields.size(); i++) {
             currentValues[i] = textFields.get(i).getText().toString();
         }
 
         try {
             if (processData(currentValues)) {
-                textFields.get(3).setText(calc() + "");
+                resultField.setText(formatResult(getResult(calc())));
             }
         } catch (IllegalArgumentException e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
@@ -62,12 +63,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void onResetClick(View v) {
         for (int i = 0; i < textFields.size(); i++) {
-            textFields.get(i).setText("");
+            textFields.get(i).setText(new String());
         }
+        resultField.setText(resultDefaultText);
         widthValue = 0;
         heightValue = 0;
         sizeValue = 0;
         resultValue = 0;
+        hasResult = false;
         Toast.makeText(this, "Resetted fields...", Toast.LENGTH_SHORT).show();
     }
 
@@ -76,15 +79,11 @@ public class MainActivity extends AppCompatActivity {
         textFields.add((EditText) findViewById(R.id.widthField));
         textFields.add((EditText) findViewById(R.id.heightField));
         textFields.add((EditText) findViewById(R.id.sizeField));
-        EditText resultField = (EditText) findViewById(R.id.resultField);
-        //resultField.setEnabled(false);
-        resultField.setKeyListener(null);
-        textFields.add(resultField);
-
+        resultField = findViewById(R.id.resultField);
+        resultDefaultText = resultField.getText().toString();
     }
 
     private boolean processData(String[] currentValues) throws IllegalArgumentException {
-        Log.d(DEBUG_TAG, currentValues.toString());
         try {
             widthValue = Long.parseLong(currentValues[0]);
             heightValue = Long.parseLong(currentValues[1]);
@@ -99,22 +98,23 @@ public class MainActivity extends AppCompatActivity {
     private double calc() throws IllegalArgumentException {
         if (widthValue < 1) {
             throw new IllegalArgumentException("The width can not be smaller than 1.");
-        }
-
-        if (heightValue < 1) {
+        } else if (heightValue < 1) {
             throw new IllegalArgumentException("The height can not be smaller than 1.");
-        }
-
-        if (sizeValue <= 0) {
+        } else if (sizeValue <= 0) {
             throw new ArithmeticException("The size can not be smaller than 1.");
-        }
-
-        double result =  Math.sqrt((widthValue * widthValue) + (heightValue * heightValue)) / sizeValue;
-
-        if (((CheckBox) findViewById(R.id.toggleRounding)).isChecked()) {
-            return (long) Math.round(result);
         } else {
-            return result;
+            resultValue = Math.sqrt((widthValue * widthValue) + (heightValue * heightValue)) / sizeValue;
+            hasResult = true;
+            return resultValue;
         }
+    }
+
+    private double getResult(double result) {
+        return roundResult ? Math.round(resultValue) : resultValue;
+    }
+
+    private String formatResult(double result) {
+        DecimalFormat format = new DecimalFormat("0.#########");
+        return format.format(result);
     }
  }
